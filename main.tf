96e9d00a1cd5f01b4f91036dae24e717cb13ae36
ErnandesSilva2 <ernandesc.silva@hotmail.com>
2025-09-17 10:19:33 -0300

    add tag update

--- main.tf
+++ main.tf
@@ -1,28 +1,29 @@
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 # Bucket S3
 
 resource "aws_s3_bucket" "project-TF" {
-    bucket = var.bucket_name
-        tags = {
+  bucket = var.bucket_name
+  tags = {
 
-            Owner = "Ernandes"
-            Env = "tf"
-        }
-    }
+    Owner = "Ernandes"
+    Env   = "tf"
+    Update = "17/9"
+  }
+}
 
 
 # EC2
 
 resource "aws_instance" "minha_ec2" {
-    ami  = var.ami_id
-    instance_type = var.instance_type
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
-    tags = {
-        Name = "teste"
-        Owner = "Ernandes"
-        Env = "TF"
-    }
+  tags = {
+    Name  = "teste"
+    Owner = "Ernandes"
+    Env   = "TF"
+  }
 }
